3d3da1bb9acaa7e564adeadc23d22129ba50845f
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2025-01-31 13:39:31 +0100

    Update description to match goal

--- buildSrc/src/main/kotlin/bungle.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/bungle.java-conventions.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 group = "io.github.thegatesdev"
 version = "1.0-SNAPSHOT"
-description = "Minecraft addon providing extra bundle functionality"
+description = "Minecraft addon providing extra inventory functionality"
 
 ext.set("id", "bungle")
 ext.set("website", "https://github.com/thegatesdev/bungle")
